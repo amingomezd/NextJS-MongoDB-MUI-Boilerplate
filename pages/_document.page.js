b02e10fb1b49fb893873188b05c2117fb232badd
amingomezd <amingomezd@gmail.com>
2023-04-21 08:56:33 -0500

    adding serverside mui config

--- pages/_document.page.js
+++ pages/_document.page.js
@@ -1,11 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import createEmotionServer from '@emotion/server/create-instance';
+import PropTypes from 'prop-types';
+import createEmotionCache from '../src/createEmotionCache';
+import theme, { roboto } from '../src/theme';
 
-export default function Document() {
+export default function MyDocument(props) {
+  const { emotionStyleTags } = props;
+
   return (
-    <Html style={{ height: '100%' }}>
+    <Html style={{ height: '100%' }} className={roboto.className}>
       <Head>
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
-        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' />
+        <meta name='theme-color' content={theme.palette.primary.main} />
+        <meta name='emotion-insertion-point' content='' />
+        {emotionStyleTags}
       </Head>
       <body style={{ height: '100%' }}>
         <Main />
@@ -14,3 +22,63 @@ export default function Document() {
     </Html>
   );
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  const originalRenderPage = ctx.renderPage;
+
+  // You can consider sharing the same Emotion cache between all the SSR requests to speed up performance.
+  // However, be aware that it can have global side effects.
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />;
+        }
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  // This is important. It prevents Emotion to render invalid HTML.
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    emotionStyleTags
+  };
+};
+
+MyDocument.propTypes = {
+  emotionStyleTags: PropTypes.array.isRequired
+};

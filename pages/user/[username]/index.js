4f7b2de3a544c82b3e23618289191a2dadc92f5b
amingomezd <amingomezd@gmail.com>
2023-04-25 10:55:30 -0500

    fix user load on profile page

--- pages/user/[username]/index.js
+++ pages/user/[username]/index.js
@@ -7,13 +7,14 @@ export default function UserPage({ user }) {
 
 export async function getServerSideProps(context) {
   const user = await findUserByUsername(context.params.username);
+
   if (!user) {
     return {
       notFound: true
     };
   }
 
-  const serializedUser = user.toObject();
+  const serializedUser = JSON.parse(JSON.stringify(user));
   serializedUser._id = String(serializedUser._id);
   return { props: { user: serializedUser } };
 }
